92e929963c7e21ffbf8b24561a104080a13fd65f
tuanld <ledangtuanbk@gmail.com>
2022-07-12 23:07:01 +0700

    init module Environment Variables

---
+++ EnvironmentVariables/src/test/java/com/ldt/environmentvariables/EnvironmentVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldt.environmentvariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentVariablesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
